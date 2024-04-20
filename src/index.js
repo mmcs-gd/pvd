1fd954ddd67282df67d281b531fff6a8d28bdf05
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2024-04-20 12:15:10 +0300

    Temporarily reverted to StartingScene

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import Phaser from 'phaser'
 
-import StartingScene from '../scenes/starting-scene';
+import StartingScene from '../scenes/starting-scene.js';
 import BulletsDemoScene from '../scenes/bullets-demo';
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  pixelArt: false,
-  zoom: 1.0,
-  scene: BulletsDemoScene,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 0,
-        debug: true // set to true to view zones
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    pixelArt: false,
+    zoom: 1.0,
+    scene: StartingScene,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 0,
+                debug: true // set to true to view zones
+            }
         }
-    }
-  },
+    },
 };
 
 const game = new Phaser.Game(config);
