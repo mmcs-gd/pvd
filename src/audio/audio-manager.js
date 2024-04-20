2bb5988a9bd9ecaaa3515be53c164b2a7b9855e1
elia-morrison <54751074+elia-morrison@users.noreply.github.com>
2024-04-20 12:58:26 +0300

    make it possible to add sounds

--- src/audio/audio-manager.js
+++ src/audio/audio-manager.js
@@ -1,4 +1,3 @@
-
 /**
  * Enum for audio types (mainly for adjusting volume).
  * @readonly
@@ -9,13 +8,65 @@ const AudioType = Object.freeze({
     Music: Symbol("music"),
 });
 
-class AudioTrack {
+class Sound {
     /** @type {string} */ id;
+    /** @type {string[]} */ assets;
     /** @type {AudioType} */ type;
     /** @type {number} */ base_volume;
     /** @type {Phaser.Sound.BaseSound | null} */ handle;
+
+    constructor(id, assets, type, base_volume) {
+        this.id = id;
+        this.assets = assets;
+        this.type = type;
+        this.base_volume = base_volume;
+        this.handle = null;
+    }
 }
 
 class AudioManager {
-    /** @type {Phaser.Game} */ #game;
+    /** @type {{[soundId: string]: Sound}} */ #soundLibrary;
+    /** @type {Phaser.Scene} */ #scene;
+
+    /*
+     * Creates a new AudioManager instance.
+     * @param {Phaser.Scene} scene The scene to manage.
+     */
+    constructor(scene) {
+        this.#scene = scene;
+        this.#soundLibrary = {};
+        this.#init_builtin_sounds();
+    }
+
+    // TODO: move this into a config
+    #init_builtin_sounds() {
+        this.addSound("gunshot", [""])
+    }
+
+    /*
+     * Adds a new sound to the library, which can later be played.
+     * @param {string} id The sound ID which you'll use later.
+     * @param {string[]} assets The list of .mp3/.ogg/other versions of the same file sound.
+     * @param {AudioType} type Type (SFX/Music).
+     * @param {number} base_volume Base sound attenuation (if sounds are not initially balanced against each other).
+     */
+    addSound(id, assets, type = AudioType.SFX, base_volume = 1.) {
+        this.#soundLibrary[id] = new Sound(
+            id = id,
+            assets = assets,
+            type = type,
+            base_volume = base_volume
+        );
+    }
+
+    /*
+     * Should be called on scene's preload to load all sounds.
+     */
+    on_preload() {
+        for (const [key, value] of Object.entries(this.#soundLibrary)) {
+            console.log(key, value);
+        }
+    }
 }
+
+export { AudioType, Sound, AudioManager }
