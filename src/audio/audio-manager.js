f09344609b8d7b43c3b58d71e21ee87f10593c73
elia-morrison <54751074+elia-morrison@users.noreply.github.com>
2024-04-20 13:56:56 +0300

    add play functionality

--- src/audio/audio-manager.js
+++ src/audio/audio-manager.js
@@ -12,12 +12,14 @@ class Sound {
     /** @type {string} */ id;
     /** @type {string[]} */ assets;
     /** @type {AudioType} */ type;
+    /** @type {boolean} */ loop;
     /** @type {number} */ base_volume;
     /** @type {Phaser.Sound.BaseSound | null} */ handle;
 
-    constructor(id, assets, type, base_volume) {
+    constructor(id, assets, loop, type, base_volume) {
         this.id = id;
         this.assets = assets;
+        this.loop = loop;
         this.type = type;
         this.base_volume = base_volume;
         this.handle = null;
@@ -25,14 +27,17 @@ class Sound {
 }
 
 class AudioManager {
+    /** @type {string} */ #assetsPath;
     /** @type {{[soundId: string]: Sound}} */ #soundLibrary;
     /** @type {Phaser.Scene} */ #scene;
 
     /*
      * Creates a new AudioManager instance.
+     * @param {string} assetsPath
      * @param {Phaser.Scene} scene The scene to manage.
      */
-    constructor(scene) {
+    constructor(assetsPath, scene) {
+        this.#assetsPath = assetsPath;
         this.#scene = scene;
         this.#soundLibrary = {};
         this.#init_builtin_sounds();
@@ -40,20 +45,23 @@ class AudioManager {
 
     // TODO: move this into a config
     #init_builtin_sounds() {
-        this.addSound("gunshot", [""])
+        let sfxpath = `${this.#assetsPath}/sfx`;
+        this.addSound("gunshot", [`${sfxpath}/gunshot.mp3`, `${sfxpath}/gunshot.ogg`], AudioType.SFX);
     }
 
     /*
      * Adds a new sound to the library, which can later be played.
      * @param {string} id The sound ID which you'll use later.
      * @param {string[]} assets The list of .mp3/.ogg/other versions of the same file sound.
+     * @param {boolean} loop Whether to loop the sound on play.
      * @param {AudioType} type Type (SFX/Music).
      * @param {number} base_volume Base sound attenuation (if sounds are not initially balanced against each other).
      */
-    addSound(id, assets, type = AudioType.SFX, base_volume = 1.) {
+    addSound(id, assets, loop = false, type = AudioType.SFX, base_volume = 1.) {
         this.#soundLibrary[id] = new Sound(
             id = id,
             assets = assets,
+            loop = loop,
             type = type,
             base_volume = base_volume
         );
@@ -64,8 +72,22 @@ class AudioManager {
      */
     on_preload() {
         for (const [key, value] of Object.entries(this.#soundLibrary)) {
-            console.log(key, value);
+            this.#scene.load.audio(key, value.assets);
         }
+    }
+
+    on_create() {
+        for (const [key, value] of Object.entries(this.#soundLibrary)) {
+            value.handle = this.#scene.sound.add(key, { loop: value.loop, volume: value.base_volume });
+        }
+    }
+
+    /*
+     * Play the sound with the given key.
+     * @param {string} key
+     */
+    play(key) {
+        this.#soundLibrary[key].handle.play();
     }
 }
 
