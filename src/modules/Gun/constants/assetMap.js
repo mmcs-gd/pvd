99df801f158e3459c670e51247b4d0827b7e4601
heyra <zalomnova.eliz@gmail.com>
2024-05-11 12:56:59 +0300

    fix: replace imports for /public assets with string paths

--- src/modules/Gun/constants/assetMap.js
+++ src/modules/Gun/constants/assetMap.js
@@ -1,13 +1,14 @@
-import { importsToMap } from 'src/utils/resource-loaders/importsToMap.js';
+// import { importsToMap } from 'src/utils/resource-loaders/importsToMap.js';
+import { pathsToImportMap } from 'src/utils/adapters/paths-to-import-map.js';
 
-const gunsPath = '/assets/sprites/pack/Characters/guns/';
+/*const gunsPath = '/assets/sprites/pack/Characters/guns/';
 
-/** @type {Record<string, string>} */
-const gunsImports = import.meta.glob('/assets/sprites/pack/Characters/guns/*.png', {
+/!** @type {Record<string, string>} *!/
+const gunsImports = import.meta.glob('/assets/sprites/pack/Characters/guns/!*.png', {
     eager: true,
     query: '?url',
     import: 'default',
-});
+});*/
 
 /**
  * @param {Record<string, string>} imports
@@ -15,4 +16,12 @@ const gunsImports = import.meta.glob('/assets/sprites/pack/Characters/guns/*.png
  * @returns {Record<string, string>}
  */
 
-export const gunsMap = importsToMap(gunsImports, gunsPath);
+const basePath = 'sprites/pack/Characters/guns';
+
+export const gunsMap = pathsToImportMap(
+    Array.from({length: 10}, (_, i) => i + 1)
+        .map(i => `${basePath}/${i}g.png?url`),
+    /(\d+)g/
+);
+
+// export const gunsMap = importsToMap(gunsImports, gunsPath);
--- src/modules/Penguin/constants/assetMap.js
+++ src/modules/Penguin/constants/assetMap.js
@@ -1,18 +1,21 @@
-import { importsToMap } from 'src/utils/resource-loaders/importsToMap.js';
+// import { importsToMap } from 'src/utils/resource-loaders/importsToMap.js';
+import { pathsToImportMap } from 'src/utils/adapters/paths-to-import-map.js';
 
-const bodiesPath = '/assets/sprites/pack/Characters/chickens/';
+/*const bodiesPath = '/assets/sprites/pack/Characters/chickens/';
 
-/** @type {Record<string, string>} */
-const bodiesImports = import.meta.glob('/assets/sprites/pack/Characters/chickens/*.png', {
+/!** @type {Record<string, string>} *!/
+const bodiesImports = import.meta.glob('/assets/sprites/pack/Characters/chickens/!*.png', {
     eager: true,
     query: '?url',
     import: 'default',
-});
+});*/
 
-/**
- * @param {Record<string, string>} imports
- * @param {string} baseUrl
- * @returns {Record<string, string>}
- */
+const bodiesPath = 'sprites/pack/Characters/chickens';
 
-export const bodiesMap = importsToMap(bodiesImports, bodiesPath);
+export const bodiesMap = pathsToImportMap(
+    Array.from({length: 10}, (_, i) => i + 1)
+        .map(i => `${bodiesPath}/${i}c.png?url`),
+    /(\d+)c/
+);
+
+// export const bodiesMap = importsToMap(bodiesImports, bodiesPath);
