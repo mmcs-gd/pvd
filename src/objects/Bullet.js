2b040c6a630951f30657bb7b72656aec918ff749
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2024-05-18 13:11:58 +0300

    Partially added types for Bullet

--- src/objects/Bullet.js
+++ src/objects/Bullet.js
@@ -1,13 +1,35 @@
-class Bullet {
-    // fallingSpeed - speed of falling bullet at the end of life time
-    constructor(scene, sprite = "bullet1", location = [400, 300], scale = 1, direction = [-1, 1], velocity = 400, distance = 400, blockedLayers = [], fallingSpeed = 1, depth = 0) {
+import Phaser from 'phaser';
+
+export default class Bullet {
+    /** @type {Phaser.Scene} */              scene;
+    /** @type {number} */                    scale;
+    /** @type {number} */                    distance;
+    /** @type {boolean} */                   destroyed;
+    /** @type {number} */                    velocity;
+    /** @type {Phaser.GameObjects.Sprite} */ sprite;
+    /** @type {number} */                    fallingSpeed;
+    /** @type {number} */                    depth;
+
+    /**
+     * @param {Phaser.Scene} scene
+     * @param {string} sprite
+     * @param {number[]} location
+     * @param {number} scale
+     * @param {number[]} direction
+     * @param {number} velocity
+     * @param {number} distance
+     * @param {any[]} blockedLayers
+     * @param {number} fallingSpeed speed of falling bullet at the end of life time
+     * @param {number} depth
+     */
+    constructor(scene, sprite = 'bullet1', location = [400, 300], scale = 1, direction = [-1, 1], velocity = 400, distance = 400, blockedLayers = [], fallingSpeed = 1, depth = 0) {
         this.scene = scene;
         this.scale = scale;
         this.distance = distance;
         this.fallingSpeed = fallingSpeed;
         this.velocity = velocity;
 
-        this.sprite = scene.add.sprite(...location, sprite);
+        this.sprite = scene.add.sprite(location[0], location[1], sprite);
         this.sprite.setDepth(depth);
         this.sprite.setOrigin(0, 0.5);
         this.sprite.rotation = Phaser.Math.Angle.Between(0, 0, direction[0], direction[1]); // rotated bullet to direction vector
@@ -30,7 +52,10 @@ class Bullet {
         });
     }
 
-    // please, I need your delta time 
+    /**
+     * Please, I need your delta time
+     * @param {number} deltaTime
+     */
     update(deltaTime) {
         this.distance -= this.velocity * deltaTime;
 
@@ -47,21 +72,27 @@ class Bullet {
         }
     }
 
+    /**
+     * TODO: wtf are these?
+     * @param {any} self
+     * @param {any} other
+     */
     onHit(self, other) {
         self.destroyed = true;
-        self.destroy();
+        self.destroy(); //
 
         // Call onHit method of other object
         try {
             other.onHit();
-        } catch (error) {
-
-        }
+        } catch (error) { /* empty */ }
     }
 }
 
+/**
+ * @param {number} value
+ * @param {number} min
+ * @param {number} max
+ */
 function clamp(value, min, max) {
     return Math.min(Math.max(value, min), max);
 }
-
-export { Bullet }
